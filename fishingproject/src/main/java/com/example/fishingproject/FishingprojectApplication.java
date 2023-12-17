90dfcfd202f369473e334e60d4368c675bc7c0f9
jrwfisher <jrwfisher@gmail.com>
2023-12-17 14:15:30 -0500

    Configured Spring Boot

---
+++ fishingproject/src/main/java/com/example/fishingproject/FishingprojectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fishingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishingprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishingprojectApplication.class, args);
+	}
+
+}
---
+++ fishingproject/src/test/java/com/example/fishingproject/FishingprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fishingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FishingprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
